8b44f6c8ec819696d6e110c57ff7167497407111
xr1m <perryar44@gmail.com>
2025-11-23 03:42:37 +0000

    Square Roots

---
+++ Square Roots/square_roots.py
@@ -0,0 +1,11 @@
+# Assigning Values:
+
+number = int(input("Enter a number: "))
+
+# Calculating the square root:
+
+answer = (number ** (0.5))
+
+# Printing the result:
+
+print("The Square Root of the number provided is: ", answer)
