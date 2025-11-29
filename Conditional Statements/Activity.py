52addc5a5cdf4e4f84a6b86a3dd1c291d776e819
xr1m <perryar44@gmail.com>
2025-11-29 15:13:12 +0000

    Conditional Statements/
    	new file:   .gitignore
    	modified:   .replit
    	new file:   Conditional Statements/Activity.py

---
+++ Conditional Statements/Activity.py
@@ -0,0 +1,8 @@
+temp_required = 20
+
+temp = int(input("Enter the current temperature: "))
+
+if temp >= temp_required:
+  print("Perfect temperature to wear light and soft clothes.")
+else:
+  print("Too cold to wear light and soft clothes.")
